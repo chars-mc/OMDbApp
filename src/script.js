7775cee4dd9c0dd9dfd4a8f2e2cea4f8b853911a
chars <chars_2000xsp@hotmail.com>
2020-07-11 16:34:24 -0500

    Hide and show menu

--- src/script.js
+++ src/script.js
@@ -3,3 +3,20 @@ import './static/icon.png';
 
 const URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=';
 const KEY = 'cea5004f';
+
+const menu = document.getElementById('menu');
+
+window.addEventListener('load', () => {
+   document.getElementById('menuButton').addEventListener('click', () => {
+      menu.classList.toggle('is-hide');
+   });
+   
+});
+
+window.addEventListener('resize', () => {
+   if(window.innerWidth >= 720) {
+      if(!menu.classList.contains('is-hide')); {
+         menu.classList.add('is-hide');
+      }
+   }
+})
