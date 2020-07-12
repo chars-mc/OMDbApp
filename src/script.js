18a35a6b41529c52899844f558e32128f72492f5
chars <chars_2000xsp@hotmail.com>
2020-07-11 19:24:41 -0500

    Hide and show search input

--- src/script.js
+++ src/script.js
@@ -5,18 +5,29 @@ const URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=';
 const KEY = 'cea5004f';
 
 const menu = document.getElementById('menu');
+const searchField = document.getElementById('searchField');
 
 window.addEventListener('load', () => {
    document.getElementById('menuButton').addEventListener('click', () => {
       menu.classList.toggle('is-hide');
+      hideElement(searchField);
    });
-   
+   document.getElementById('searchButton').addEventListener('click', (e) => {
+      e.preventDefault();
+      searchField.classList.toggle('is-hide');
+      hideElement(menu);
+   });
+
 });
 
 window.addEventListener('resize', () => {
    if(window.innerWidth >= 720) {
-      if(!menu.classList.contains('is-hide')); {
-         menu.classList.add('is-hide');
-      }
+      hideElement(menu);
+      hideElement(searchField);
    }
-})
+});
+
+function hideElement(element) {
+   if(!element.classList.contains('is-hide'))
+      element.classList.add('is-hide');
+}
