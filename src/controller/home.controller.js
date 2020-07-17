d172aee269b2d0aeda0dc6a10e33a16fd69c4fcb
chars <chars_2000xsp@hotmail.com>
2020-07-16 20:26:24 -0500

    Show movies

--- src/controller/home.controller.js
+++ src/controller/home.controller.js
@@ -7,10 +7,18 @@ let movies = [];
 
 export default async () => {
    const div = document.createElement('div');
+   let fragment = document.createDocumentFragment();
+
    div.innerHTML += view;
-   
    movies = await getMovies();
-   console.log(movies)
+
+   // console.log(movies);
+   movies.Search.forEach(movie => {
+      fragment.appendChild(printMovie(movie));
+      console.log(movie)
+   });
+
+   div.querySelector('#movies').appendChild(fragment);
    
    return div;
 };
@@ -20,4 +28,24 @@ async function getMovies(search = 'robot') {
    const dataJSON = await data.json();
 
    return dataJSON;
+}
+
+function printMovie(movie) {
+   const movieDiv = document.createElement('div');
+   movieDiv.className = 'movie';
+   movieDiv.innerHTML = /*html*/ `
+      <img src="${movie.Poster}" alt="${movie.Title}" class="movie__poster">
+      <div class="movie__details">
+         <h4 class="movie__title">${movie.Title}</h4>
+         <div class="movie__actions">
+            <a href="#/movie/${movie.Title}" class="movie__read-more">READ MORE
+               <i class="material-icons">arrow_right</i>
+            </a>
+            <button class="movie__add-favorite button">
+               <i class="material-icons">star_border</i>
+            </button>
+         </div>
+      </div>`;
+
+   return movieDiv;
 }
