8bd817f31563fc86ade482d37d9ce47de0fc8626
chars <chars_2000xsp@hotmail.com>
2020-07-17 11:41:59 -0500

    Fix background of menu

--- src/script.js
+++ src/script.js
@@ -19,11 +19,15 @@ window.addEventListener('load', () => {
    document.getElementById('menuButton').addEventListener('click', () => {
       menu.classList.toggle('is-hide');
       hideElement(searchField);
+      removeBlurBackground();
+      addBlurBackground();
    });
    document.getElementById('searchButton').addEventListener('click', (e) => {
       e.preventDefault();
       searchField.classList.toggle('is-hide');
       hideElement(menu);
+      removeBlurBackground();
+      addBlurBackground();
    });
 
 });
@@ -32,5 +36,21 @@ window.addEventListener('resize', () => {
    if(window.innerWidth >= 720) {
       hideElement(menu);
       hideElement(searchField);
+      removeBlurBackground();
    }
 });
+
+function addBlurBackground() {
+   const root = document.getElementById('root');
+
+   if(!hasClass(menu, 'is-hide') || !hasClass(searchField, 'is-hide') && !hasClass(root, 'is-blur')) root.classList.add('is-blur');
+      else removeBlurBackground();
+}
+
+function removeBlurBackground() {
+   document.getElementById('root').classList.remove('is-blur');
+}
+
+function hasClass(element, classString) {
+   return element.classList.contains(classString);
+}
